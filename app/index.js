6ced0ca29cb1723716d55fcab1c358739264eeb5
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-08-15 14:59:35 +0300

    Application requires Notification permissions.

--- app/index.js
+++ app/index.js
@@ -1,23 +1,43 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./service-worker.js")
-      .then(registration => {
-        console.log("SW registered: ", registration);
-      })
-      .catch(registrationError => {
-        console.log("SW registration failed: ", registrationError);
-      });
+    loadServiceWorker();
 
     unregisterOldVersions();
+
+    getNotificationPermissions();
   });
 }
 
+function loadServiceWorker() {
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then(registration => {
+      console.log("SW registered: ", registration);
+    })
+    .catch(registrationError => {
+      console.log("SW registration failed: ", registrationError);
+    });
+}
+
 function unregisterOldVersions() {
-  console.log('Unregister old service workers')
+  console.log("Unregister old service workers");
   navigator.serviceWorker.getRegistrations().then(registrations => {
     for (let registration of registrations) {
       registration.unregister().then(unregister => console.log(unregister));
     }
   });
+}
+
+function getNotificationPermissions() {
+  if (!"Notification" in window) {
+    // If the browser version is unsupported, remain silent.
+    alert("Browser doesn't support push notifications ...");
+
+    return;
+  }
+
+
+  Notification.requestPermission(status => {
+    console.log('Notification permission status:', status)
+  })
 }
