eb9c8adb6697f6eea24bc16c4a9bff367fbc4b96
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-08-19 13:41:45 +0300

    Backend service is able to send push notification.

--- backend/notification.js
+++ backend/notification.js
@@ -1,1 +1,24 @@
-console.log('Hello world !!!')
+require('dotenv').config()
+const webPush = require('web-push')
+const pushSubscription = require('./pushSubscription.json')
+
+const options = {
+	TTL: 60,
+	vapidDetails: {
+    subject: `mailto:${process.env.VAPID_EMAIL}`,
+    publicKey: process.env.VAPID_PUBLIC_KEY,
+    privateKey: process.env.VAPID_PRIVATE_KEY
+  }
+}
+
+const date = new Date()
+const payload = `Push notification: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+
+webPush.sendNotification(
+  pushSubscription,
+  payload,
+  options
+)
+
+console.log(options)
+
