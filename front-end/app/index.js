f2a03348719d639778f7cadf19947e341ff7752b
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-11-24 20:24:17 +0200

    getNotificationData is an external method.

--- front-end/app/index.js
+++ front-end/app/index.js
@@ -5,6 +5,8 @@ import pushNotificationVersion from './push-api/pushNotificationVersion';
 import NativeClient from './push-api/native-client';
 import SafariPushNotificationClient from './push-api/safari-client';
 
+import getNotificationData from './dummy-data/getNotificationData';
+
 require('dotenv').config();
 
 const pushNotificationClient = pushNotificationVersion();
@@ -67,62 +69,6 @@ function createNewNotificationMessage() {
   n.onclose = function() {
     console.log('Notification closed');
   };
-}
-
-function getNotificationData() {
-  const title = 'New message from Dimitar';
-  const options = {
-    body: "Dimitar: I'm a developer",
-
-    // ...prevent duplicate notifications
-    tag: generateUniqueTag(),
-
-    // There are scenarios where you might want a replacing notification to notify the user rather than silently update. Chat applications are a good example.
-    // In this case, you should set tag and renotify to true.
-    renotify: true,
-
-    data: {
-      // Lets us identity notification
-      primaryKey: 1,
-    },
-  };
-
-  if ('actions' in Notification.prototype) {
-    console.log('Action buttons are supported.');
-    options.actions = getActions();
-  } else {
-    console.log('Action buttons are NOT supported.');
-  }
-
-  return {
-    title,
-    options,
-  };
-}
-
-function generateUniqueTag() {
-  return `unique-string-${new Date().toString()}`;
-}
-
-function getActions() {
-  return [
-    {
-      action: 'react-action',
-      title: 'React',
-    },
-    {
-      action: 'angular-action',
-      title: 'Angular',
-    },
-    {
-      action: 'vuejs-action',
-      title: 'Vuejs',
-    },
-    {
-      action: 'ember-action',
-      title: 'Emberjs',
-    },
-  ];
 }
 
 /** * Buttons  ** */
